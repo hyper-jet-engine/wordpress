d55dd2b92a51cee15e0aabb0aa88111d93923151
Thunder Analytics Ltd <57893116+thunder-analytics-ltd@users.noreply.github.com>
2022-06-12 22:48:12 +0200

    Add files via upload

---
+++ includes/class-cache-panel-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       system-admin.com
+ * @since      1.0.0
+ *
+ * @package    Cache_Panel
+ * @subpackage Cache_Panel/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Cache_Panel
+ * @subpackage Cache_Panel/includes
+ * @author     System Admin <system-admin@mail.com>
+ */
+class Cache_Panel_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-cache-panel-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       system-admin.com
+ * @since      1.0.0
+ *
+ * @package    Cache_Panel
+ * @subpackage Cache_Panel/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cache_Panel
+ * @subpackage Cache_Panel/includes
+ * @author     System Admin <system-admin@mail.com>
+ */
+class Cache_Panel_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
